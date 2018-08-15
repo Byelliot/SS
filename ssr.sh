2c79e8af70b7514763467a7041ff5ec655e44506
Byelliot <39994633+Byelliot@users.noreply.github.com>
2018-08-15 18:57:26 +0800

    一键脚本
    添加

---
+++ ssr.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# Auto install ssr
+#
+# System Required:  CentOS 7
+#
+# Copyright (C) 2018 ECloud <admin@byelliot.top>
+#
+# URL: https://ss.byellio.top
+#
+
+echo "Press any key to start...or Press Ctrl+C to cancel"
+
+char=`get_char`
+
+
+yum -y groupinstall "Development Tools"
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+./configure && make -j2 && make install
+echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+ldconfig
+cd /root
+yum -y install python-setuptools
+easy_install pip
+git clone -b manyuser https://github.com/glzjin/shadowsocks.git
+cd shadowsocks
+pip install -r requirements.txt
+cp apiconfig.py userapiconfig.py
+cp config.json user-config.json
+
+echo "已完成"
