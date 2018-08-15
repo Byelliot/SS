19277e153c4188392d93096cbda46a2da1ac7ad9
Byelliot <39994633+Byelliot@users.noreply.github.com>
2018-08-15 19:12:01 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -8,6 +8,16 @@
 #
 # URL: https://ss.byellio.top
 #
+get_char() 
+{ 
+SAVEDSTTY=`stty -g` 
+stty -echo 
+stty cbreak 
+dd if=/dev/tty bs=1 count=1 2> /dev/null 
+stty -raw 
+stty echo 
+stty $SAVEDSTTY 
+} 
 
 echo "Press any key to start...or Press Ctrl+C to cancel"
 
