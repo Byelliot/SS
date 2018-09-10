7d696f199f08e3498f03be02834bf04c50c5dd94
Byelliot <39994633+Byelliot@users.noreply.github.com>
2018-09-10 18:07:56 +0800

    更新9.10

--- run.sh
+++ run.sh
@@ -24,6 +24,7 @@ echo "Press any key to start...or Press Ctrl+C to cancel"
 char=`get_char` 
 
 systemctl stop firewalld.service
+systemctl disable firewalld.service
 cd /root/shadowsocks
 ./run.sh
 
