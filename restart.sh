9068f91cb343c4b0a4e7d168642a156a904ea4bb
Byelliot <39994633+Byelliot@users.noreply.github.com>
2020-08-02 17:43:28 +0800

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Auto install ssr
+#
+# System Required:  CentOS 7
+#
+# Copyright (C) 2018 ECloud <admin@byelliot.top>
+#
+# URL: https://ss.byellio.top
+#
+
+docker restart ssrmu
